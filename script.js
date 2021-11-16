7a90efec9df9690c9b8dec82e70ad309140f2630
JeanPierre <jp1966.adoux@gmail.com>
2021-11-16 16:58:08 +0100

    Exercice Chifoumi le 16/11/2021 suite

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const myKeyboard = document.getElementById("myKeyboard");
-const indexLetter = document.getElementById("indexLetter");
-const farmList = [
-    "Poule",
-    "Lapin",
-    "Dindon",
-    "Coq",
-    "Vache"
-];
-let cptChar = 0;
-let nbLife = 10;
+const myChifoumi = ["Pierre", "Feuille", "Ciseaux"];
+const myAleaChifoumi = document.getElementById("myAleaChifoumi");
+const indexChifoumi = document.getElementById("indexChifoumi");
 
-alphabet.forEach(Element => {
-    let myLetter = Element;
-    myLetter = myLetter.toUpperCase();
-    let myKey = `<button class ="m-2 btn btn-outline-primary col-2 id="myKey${myLetter}">${myLetter}</button>`;
-    myKeyboard.innerHTML += (myKey);
-});
+let nbLife = 10;
 
 function aleaNb(min, max) {
     min = Math.ceil(min);
@@ -24,39 +10,14 @@ function aleaNb(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+let choiceAleaThing = myChifoumi[aleaNb(0, myChifoumi.length)];
+console.log(choiceAleaThing);
 
-let nomAnimal = farmList[aleaNb(0, farmList.length)];
-let tabChar = nomAnimal.split('');
-tabChar.forEach(Element => {
-    let myLetterDisplay = Element;
-    let displayChar = `<div class="row myLet justify-content-center border border-dark rounded text-center col-2" id="indexLetter${myLetterDisplay}">_</div>`;
-    indexLetter.innerHTML += (displayChar);
-});
+function RecupChoix(choixPlayer) {
+   console.log(choixPlayer); 
+}
 
-function Action_Jouer() {
-    location.reload();
-};
-
-myKeyboard.addEventListener("click", (e) => {
-    if (e.target.nodeName == "BUTTON") {
-        let myScore = cptChar;
-        e.target.className = "m-2 btn btn-primary col-2";
-        e.target.disabled = true;
-        tabChar.forEach((Element, index) => {
-            console.log(e.target);
-            if (e.target.innertext.toLowerCase() == Element) {
-                document.getElementById(`indexLetter${index}`).innerText = Element;
-            cptChar++;
-            }
-        })
-    }
-});
-
-if (cptChar == tabChar.length) {
-    window.confirm("Gagné, voulez-vous rejouer?")
-};
-
-switch (nbLife) {
+    switch (nbLife) {
     case 0:
         confirm("C'est perdu, voulez-vous rejouer?")
         break;
