f25a14d51f00af9c449ce3f544f0f6d82a61428c
JeanPierre <jp1966.adoux@gmail.com>
2021-11-17 12:34:53 +0100

    Exercice Chifoumi le 17/11/2021 suite

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const myChifoumi = ["Pierre", "Feuille", "Ciseaux"];
 const myAleaChifoumi = document.getElementById("myAleaChifoumi");
 const indexChifoumi = document.getElementById("indexChifoumi");
-
-let nbLife = 10;
+let scorePlayer = 0;
+let scoreComputer = 0;
+let nbGame = 0;
 
 function aleaNb(min, max) {
     min = Math.ceil(min);
@@ -10,20 +11,85 @@ function aleaNb(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
-let choiceAleaThing = myChifoumi[aleaNb(0, myChifoumi.length)];
-console.log(choiceAleaThing);
+function recupChoix() {
+    console.log();
+    let choixPlayer = [1];
+    console.log(choixPlayer);
+};
 
-function RecupChoix(choixPlayer) {
-   console.log(choixPlayer); 
-}
+function choixaleaNb() {
+    let choiceAleaThing = myChifoumi[aleaNb(0, myChifoumi.length)];
+    console.log(choiceAleaThing);
+};
 
-    switch (nbLife) {
-    case 0:
-        confirm("C'est perdu, voulez-vous rejouer?")
-        break;
+function combat() {
+    if (nbGame == 10) {
+        if (scorePlayer > scoreComputer) {
+            window.confirm(" Vainqueur le joueur - Score du joueur = " + scorePlayer + " Score de l ordinateur = " + scoreComputer);
+            location.reload();
+        } else if (scorePlayer = scoreComputer) {
+            window.confirm(" Match nul - Score du joueur = " + scorePlayer + " Score de l ordinateur = " + scoreComputer);
+            location.reload();
+        } else {
+            window.confirm(" Vainqueur l ordinateur - Score du joueur = " + scorePlayer + " Score de l ordinateur = " + scoreComputer);
+            location.reload();
+        }
+    } else {
+        choixaleaNb();
+        recupChoix();
+        resultat();
+        nbGame++;
+        console.log(nbGame);
+    }
+};
 
-    default:
-        break;
+function resultat(choixPlayer) {
+    if (choixPlayer == "Pierre" && choiceAleaThing == "Pierre") {
+        sendMessage(nul);
+    } else {
+        if (choixPlayer == "Pierre" && choiceAleaThing == "Ciseaux") {
+            sendMessage(gagné);
+            scorePlayer++;
+        } else {
+            if (choixPlayer == "Pierre" && choiceAleaThing == "Feuille") {
+                sendMessage(perdu);
+                scoreComputer++;
+            } else {
+                if (choixPlayer == "Feuille" && choiceAleaThing == "Ciseaux") {
+                    sendMessage(perdu);
+                    scoreComputer++;
+                } else {
+                    if (choixPlayer == "Feuille" && choiceAleaThing == "Pierre") {
+                        sendMessage(gagné);
+                        scorePlayer++;
+                    } else {
+                        if (choixPlayer == "Feuille" && choiceAleaThing == "Feuille") {
+                            sendMessage(nul);
+                        } else {
+                            if (choixPlayer == "Ciseaux" && choiceAleaThing == "Feuille") {
+                                sendMessage(gagné);
+                            } else {
+                                if (choixPlayer == "Ciseaux" && choiceAleaThing == "Pierre") {
+                                    sendMessage(perdu);
+                                } else {
+                                    if (choixPlayer == "Ciseaux" && choiceAleaThing == "Ciseaux") {
+                                        sendMessage(nul);
+                                    } else {
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+function sendMessage(msg) {
+    window.confirm(choixPlayer + " contre " + choiceAleaThing + " = match " + msg);
+    console.log(sendMessage(nul));
 };
 
 
